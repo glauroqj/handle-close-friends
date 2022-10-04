2cced842deaa488fac04d3fdca2bf99ae87f276e
glauroqj <glauro.juliani@hotmail.com>
2022-10-04 23:31:47 +0200

    add

--- pages/api/auth/instagram-token.js
+++ pages/api/auth/instagram-token.js
@@ -26,21 +26,47 @@ export default async function handler(req, res) {
   }
 
 
-  // try {
-  //   const responseRaw = await fetch('https://api.instagram.com/oauth/access_token', {
-  //     headers: {
-  //       client_id: '',
-  //       client_secret: ''
-  //     }
-  //   });
-  // } catch (e) {
-  //   res.status(400).send({
-  //     code: 400,
-  //     error: true,
-  //     message: 'error',
-  //     location: 'instagram-token'
-  //   })
-  //   return false
-  // }
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const payload = new FormData()
+    payload.append('client_id', '3217961178471425')
+    payload.append('client_secret', 'd14a930dc50462ab077a9b6e1024a446')
+    payload.append('code', token)
+    payload.append('grant_type', 'authorization_code')
+    payload.append('redirect_uri', 'https://handle-close-friends.com.br:5000/auth/instagram')
+
+    // {
+    //   'client_id': '3217961178471425',
+    //   'client_secret': 'd14a930dc50462ab077a9b6e1024a446',
+    //   'code': String(token),
+    //   'grant_type': 'authorization_code',
+    //   'redirect_uri': 'https://handle-close-friends.com.br:5000/'
+    // },
+
+    const responseRaw = await fetch('https://api.instagram.com/oauth/access_token', {
+      method: 'post',
+      body: payload
+    });
+
+    const response = await responseRaw.json()
+
+    console.log('< RESPONSE INSTAGRAM > ', responseRaw?.status)
+
+    console.log('< RESPONSE > ', response)
+    if (responseRaw.status === 200) {
+      res.status(200).json({
+        ...response
+      })
+    } else {
+      res.status(400).json({ ...response, error: true })
+    }
+  } catch (e) {
+    res.status(400).send({
+      code: 400,
+      error: true,
+      message: 'error',
+      location: 'instagram-token'
+    })
+    return false
+  }
+  // res.status(200).json({ name: 'John Doe' })
 }
