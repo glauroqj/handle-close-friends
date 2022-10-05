7059a3cd6e68486eeedaee4b6ec5c2a2c2c2b0ee
glauroqj <glauro.juliani@hotmail.com>
2022-10-05 21:20:22 +0200

    add: theme style

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 /** ssr material themes */
 import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@mui/material/styles'
+import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
 
 class MyDocument extends Document {
   render() {
